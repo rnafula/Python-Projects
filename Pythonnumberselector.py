fe5f01b7c7d3ce8f3608c436c233e5cc4177c9aa
rnafula <rachaelsikuku033@gmail.com>
2024-08-09 07:51:42 +0300

    Number game first release!

---
+++ Pythonnumberselector.py
@@ -0,0 +1,27 @@
+import random
+import math
+
+lower = int(input("Enter lower bound:-"))
+upper = int(input("Enter upper bound:-"))
+x = random.randrange(lower,upper)
+total_chances = math.ceil(math.log(upper - lower + 1,2))
+print("\n\tYou've only ", total_chances, " chances to guess the integer! \n")
+
+count = 0
+flag =False
+
+while count < total_chances:
+    count += 1
+    guess = int(input("Guess a number:-"))
+    if x == guess:
+        print ("Congratulations you did it in", count, "try")
+        flag = True
+        break
+    elif x > guess:
+        print("You guessed too small!")
+    elif x < guess:
+        print("You guessed to high!")   
+
+if not flag:
+    print("\nThe number is %d" % x)
+    print("\tBetter Luck Next time!")         
